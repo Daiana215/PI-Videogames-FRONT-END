abb6a57e8431742064ec2c95eb50e073f0bab86a
Daiana <daiana_agustina@outlook.com>
2022-05-02 23:42:33 -0300

    Add files via upload

---
+++ src/Styles/landing.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Contenedor = styled.div`
+    border: transparent;
+    width: 50%;
+    height: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    margin-left: 20rem;
+    div a{ 
+        display: flex;
+        justify-content: center;
+        border: transparent;
+        margin-bottom: 3rem;
+    }
+`
